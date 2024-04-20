84f8c88ea043001b3453034c573fc6f7876149bc
Lucas Gray <94577382+lucas-gray@users.noreply.github.com>
2024-04-20 02:40:02 -0600

    Edit polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,8 +3,6 @@
 # Terminate already running bar instances
 killall -q polybar
 
-#polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
-
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload mybar &
