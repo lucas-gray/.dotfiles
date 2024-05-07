9d5fb0395502f91e709c4a6e912d51e7bd32fb38
Lucas Gray <94577382+lucas-gray@users.noreply.github.com>
2024-05-07 05:17:19 -0600

    Add small edits

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,6 +3,7 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# Start Polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload mybar &
