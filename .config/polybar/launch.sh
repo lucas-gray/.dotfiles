1aee37042c31be6a62c6bb676ca653b8a3d2ab65
Lucas Gray <94577382+lucas-gray@users.noreply.github.com>
2025-01-18 13:04:46 -0700

    Update Polybar config to always show the system tray on the correct monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Start Polybar
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
-  done
-else
-  polybar --reload mybar &
-fi
+polybar --reload mainbar &
+polybar --reload sidebar &
+
+# Old way of starting that doesn't work well with two monitors and system tray
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload mainbar &
+#  done
+#else
+#  polybar --reload mainbar &
+#fi
 
 echo "Polybar launched..."
