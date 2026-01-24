6d3fe98dacbd376d7e19bc2459c20151301f63c2
Lucas Gray <94577382+lucas-gray@users.noreply.github.com>
2026-01-23 20:04:38 -0700

    Enable transparent background in Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -65,7 +65,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
@@ -702,6 +702,11 @@ require('lazy').setup({
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+    opts = {
+      transparent = true,
+      sidebars = 'transparent',
+      floats = 'transparent',
+    },
   },
 
   -- Highlight todo, notes, etc in comments
